e2aafbd5079ed8b816f47a5aa9d1f6b226b837bc
Phanuelkb <145259421+Phanuelkb@users.noreply.github.com>
2023-09-17 16:25:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,14 +3,13 @@ function validateForm() {
     var email = document.getElementById("email").value;
     var phone = document.getElementById("phone").value;
 
-    // Add more validation for other fields if needed
+  
 
     if (name === "" || email === "" || phone === "") {
         alert("Please fill in all required fields.");
         return false;
     }
 
-    // Add more specific validation rules here
 
     return true;
 }
@@ -19,14 +18,11 @@ function validateLoginForm() {
     var username = document.getElementById("username").value;
     var password = document.getElementById("password").value;
 
-    // Add validation rules for login fields
 
     if (username === "" || password === "") {
         alert("Please fill in both username and password.");
         return false;
     }
-
-    // Add more specific validation rules here
 
     return true;
 }
